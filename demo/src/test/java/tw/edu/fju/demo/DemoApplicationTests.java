1ec95bd33670f27dcbb8b8fde4a8d153aec9fbea
Eason-0302 <bingyu0302@gmail.com>
2026-07-05 22:22:43 +0800

    [NO-AI] 設定 Spring Profiles：dev（SQLite）/ prod（PostgreSQL）分離，加入 PostgreSQL 依賴

--- demo/src/test/java/tw/edu/fju/demo/DemoApplicationTests.java
+++ demo/src/test/java/tw/edu/fju/demo/DemoApplicationTests.java
@@ -2,8 +2,9 @@ package tw.edu.fju.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import tw.edu.fju.miniclinic.miniclinicApplication;
 
-@SpringBootTest
+@SpringBootTest(classes = miniclinicApplication.class)
 class DemoApplicationTests {
 
 	@Test
